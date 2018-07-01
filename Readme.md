fb058996af0cbb951c4d9d846ff64defff51907f
Filipe Ferreira <lipe.ferreira1609@gmail.com>
2018-07-01 01:21:58 -0300

    :D trabalhando com regex

---
+++ Readme.md
@@ -0,0 +1,27 @@
+# Um pouco de Regex com Python!
+
+O Arquivo "python expressoes_regulares_python.py" mostra um pouco da implementação do Regex no python.
+
+
+ [^ ] - não tem espaço
+ * - tudo
+ [^ ]* - tudo que não tenha espaço
+ [r|R] - Tenha uma letra R no meio(maiúscula ou minúscula)
+ [^|\.] - Não tenha espaço ou . no final(descape \.)
+ 
+ 
+ re.search(), este método pesquisa e retorna primeira instância encontrada usando expressões regex
+ 
+ re.findall(), retorna uma lista com todos os resultados encontrados através da expressão regular
+ 
+ re.split(), retorna lista criada através do caracter de corte na string
+ 
+ # Dúvidas? - Veja a implementação destes métodos no arquivo "python expressoes_regulares_python.py" ele está neste repositório mesmo :D
+ 
+
+
+##### para rodar basta ter o Python instalado em sua máquina e executar com o mesmo. EX.: no seu terminal digite => 'python expressoes_regulares_python.py'
+
+
+### Ilustração 01
+![Ilustracao 01](https://image.ibb.co/ijLiXJ/ilustracao.png)
---
+++ expressoes_regulares_python.py
@@ -0,0 +1,47 @@
+﻿# -*- coding: UTF-8 -*-
+
+# [^ ] - não tem espaço
+# * - tudo
+# [^ ]* - tudo que não tenha espaço
+# [r|R] - Tenha uma letra R no meio(maiúscula ou minúscula)
+# [^|\.] - Não tenha espaço ou . no final(descape \.)
+
+
+import re
+
+
+
+## ---- search, este método pesquisa e retorna primeira instancia encontrada usando expressões regex
+frase = "O rato roeu a roupa do rei de roma."
+
+busca = re.search("[^ ]*[r|R][^ |\.]*", frase)
+
+if busca:
+	print("Primeira palavra da busca acima com search: ",busca.group())
+	
+
+	
+
+## ---- findAll, retorna uma lista com todos os resultados encontrados através da expressão regular
+busca = re.findall("[^ ]*[r|R][^ |\.]*", frase)
+
+if busca:
+	print("Todas as palavras da busca acima com findall: ",busca)
+	
+	
+	
+	
+## ---- Split, retorna lista criada através do caracter de corte na frase
+busca = re.split(" ", frase)
+
+if busca:
+	print("split: ",busca)
+	
+	
+	
+	
+## ---- Procurar email na frase
+busca = re.findall("[A-Za-z0-9]*[@][^ ]*[^ \)]", "Meu email é lipe2018@gmail.com e sempre olho a caixa de mensagem!(lipe2016@gmail)")
+
+if busca:
+	print("pesquisar email(s) em frase: ",busca)
